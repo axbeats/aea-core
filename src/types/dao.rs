7e8e6dc93525e2bdeaa2f34f1cc88aaa327a5e64
Axel Whalen <axel.whalen@pangea.art>
2025-03-07 22:02:16 -0800

    rewrite deploy dao

--- src/types/dao.rs
+++ src/types/dao.rs
@@ -6,9 +6,28 @@ pub type DaoId = AccountId;
 #[derive(Debug, Clone)]
 pub struct DaoInput {
     pub profile: ProfileInput,
-    pub groups: Vec<GroupInput>,
     pub default_policy: Policy,
-    pub token: Option<TokenInput>,
+    pub groups: Vec<GroupInput>,
+    pub contracts: Vec<ContractInput>,
+    pub rules: Vec<RuleInput>,
+}
+
+#[near(serializers = [json, borsh])]
+#[derive(Debug, Clone, PartialEq)]
+pub enum DaoSetupPhase {
+    Init,
+    Profile,
+    Groups,
+    Contracts,
+    Rules,
+    Active,
+    Failed,
+}
+
+impl Default for DaoSetupPhase {
+    fn default() -> Self {
+        Self::Init
+    }
 }
 
 #[near(serializers = [json, borsh])]
@@ -19,6 +38,8 @@ pub struct DaoConfig {
     pub token_id: Option<TokenContractId>,
     pub staking_id: Option<StakingId>,
     pub metadata: ContractMetadata,
+    pub setup_phase: DaoSetupPhase,
+    pub setup_error: Option<String>,
 }
 
 #[near(serializers = [json, borsh])]
